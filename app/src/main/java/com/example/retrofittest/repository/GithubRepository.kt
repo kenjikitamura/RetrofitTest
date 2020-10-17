a4c75ebc0ab070c0e16836461e0303767a79a17d
Kenji Kitamura <kenji@rainbowdeivl.jp>
2020-10-17 21:39:49 +0900

    エラー処理

--- app/src/main/java/com/example/retrofittest/repository/GithubRepository.kt
+++ app/src/main/java/com/example/retrofittest/repository/GithubRepository.kt
@@ -1,10 +1,17 @@
 package com.example.retrofittest.repository
 
+import android.util.Log
 import com.example.retrofittest.api.GithubApi
 import com.example.retrofittest.entity.SearchRepositoriesResult
+import retrofit2.HttpException
 
 class GithubRepository(val githubApi: GithubApi) {
     suspend fun getSearchRepositories() : SearchRepositoriesResult {
-        return githubApi.getSearchRepositories("kotlin")
+        try {
+            return githubApi.getSearchRepositories("kotlin")
+        } catch (e: HttpException) {
+            Log.e("Test", "error!!!", e)
+        }
+        return SearchRepositoriesResult(999, emptyList())
     }
 }
