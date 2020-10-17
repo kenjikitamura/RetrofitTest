75b240b67b4090cce3ed5a5265e5805aa0ecb33b
Kenji Kitamura <kenji@rainbowdeivl.jp>
2020-10-18 08:57:41 +0900

    Proxyを入れたが、suspend関数のProxyはそのままだとできない？

--- app/src/main/java/com/example/retrofittest/FirstViewModel.kt
+++ app/src/main/java/com/example/retrofittest/FirstViewModel.kt
@@ -3,46 +3,12 @@ package com.example.retrofittest
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.retrofittest.repository.GithubRepository
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Method
-import java.lang.reflect.Proxy
 
 class FirstViewModel(
     private val githubRepository: GithubRepository
 ): ViewModel() {
     suspend fun test() {
-
-        val test = TestImpl()
-        val proxy = Proxy.newProxyInstance(
-            Test::class.java.classLoader,
-            arrayOf(Test::class.java),
-            Handler(test)
-        ) as Test
-        proxy.test()
         val result = githubRepository.getSearchRepositories()
         Log.d("Test", "TEST!!! $result")
-    }
-
-    class Handler(val obj: Any): InvocationHandler {
-        override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any? {
-            Log.d("Test", "Before $args")
-            val ret = if (args == null) {
-                method!!.invoke(obj)
-            } else {
-                method!!.invoke(obj, *args)
-            }
-            Log.d("Test", "After")
-            return ret
-        }
-    }
-}
-
-interface Test {
-    fun test()
-}
-
-class TestImpl: Test {
-    override fun test() {
-        Log.d("Test","HOGEHOGE!!")
     }
 }
--- app/src/main/java/com/example/retrofittest/MyApplication.kt
+++ app/src/main/java/com/example/retrofittest/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.example.retrofittest
 
 import android.app.Application
+import android.util.Log
 import com.example.retrofittest.api.GithubApi
 import com.example.retrofittest.repository.GithubRepository
 import com.squareup.moshi.Moshi
@@ -10,8 +11,14 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.lang.Exception
+import java.lang.IllegalStateException
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
 import java.util.concurrent.TimeUnit
 
 class MyApplication : Application() {
@@ -51,10 +58,44 @@ class MyApplication : Application() {
         // GithubApi
         single {
             val retrofit = get<Retrofit>()
-            val api = retrofit.create(GithubApi::class.java)
+            val api = Converter<GithubApi>().conv(retrofit, GithubApi::class.java)
             GithubRepository(api)
         }
 
         viewModel { FirstViewModel(get()) }
     }
+
+    class Converter<T> {
+
+        @Suppress("UNCHECKED_CAST")
+        fun conv(retrofit: Retrofit, clazz: Class<T>): T {
+            val api = retrofit.create(clazz)
+            val proxy = Proxy.newProxyInstance(
+                clazz.classLoader,
+                arrayOf(clazz),
+                Handler(api)
+            )
+
+            return proxy as? T ?: throw IllegalStateException("proxy cannot cast target class type.")
+        }
+
+        inner class Handler(private val obj: T): InvocationHandler {
+            override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any? {
+                Log.d("Test", "Before method=$method args=$args")
+                try {
+                    val ret = if (args == null) {
+                        method!!.invoke(obj)
+                    } else {
+                        method!!.invoke(obj, *args)
+                    }
+                    Log.d("Test", "After $ret")
+                    return ret
+                } catch (e: Throwable) {
+                    throw MyException(e)
+                }
+            }
+        }
+    }
 }
+
+class MyException(e: Throwable): Exception(e)
